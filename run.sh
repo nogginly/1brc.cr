10d6a2369131d293ca4e8bb6b5ba578407346830
nogginly <nogginly@icloud.com>
2024-02-05 21:44:49 -0500

    Fix to be runnable from anywhere

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 # $2 thread count
 # $3 buffer division
 
-CRYSTAL_WORKERS=$2 BUF_DIV_DENOM=$3 bin/$1 measurements.txt out/$1_D$2_B$3.txt
+CRYSTAL_WORKERS=$2 BUF_DIV_DENOM=$3 $(dirname $0)/bin/$1 measurements.txt $(dirname $0)/out/$1_D$2_B$3.txt
